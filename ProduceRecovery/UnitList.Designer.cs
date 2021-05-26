2bf712560c52ab984f72ace5a78a8365617ec4d5
noawar <noawar.ir@gmail.com>
2021-05-26 17:50:31 +0430

    unitlist form first design

---
+++ ProduceRecovery/UnitList.Designer.cs
@@ -0,0 +1,263 @@
+﻿
+namespace ProduceRecovery
+{
+    partial class UnitList
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(UnitList));
+            this.barManager1 = new DevExpress.XtraBars.BarManager(this.components);
+            this.bar1 = new DevExpress.XtraBars.Bar();
+            this.bar3 = new DevExpress.XtraBars.Bar();
+            this.barDockControlTop = new DevExpress.XtraBars.BarDockControl();
+            this.barDockControlBottom = new DevExpress.XtraBars.BarDockControl();
+            this.barDockControlLeft = new DevExpress.XtraBars.BarDockControl();
+            this.barDockControlRight = new DevExpress.XtraBars.BarDockControl();
+            this.addNewBtn = new DevExpress.XtraBars.BarButtonItem();
+            this.editBtn = new DevExpress.XtraBars.BarButtonItem();
+            this.trashBtn = new DevExpress.XtraBars.BarButtonItem();
+            this.deleteBtn = new DevExpress.XtraBars.BarButtonItem();
+            this.refreshBtn = new DevExpress.XtraBars.BarButtonItem();
+            this.xlsBtn = new DevExpress.XtraBars.BarButtonItem();
+            this.printBtn = new DevExpress.XtraBars.BarButtonItem();
+            this.gc = new DevExpress.XtraGrid.GridControl();
+            this.gv = new DevExpress.XtraGrid.Views.Grid.GridView();
+            ((System.ComponentModel.ISupportInitialize)(this.barManager1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gc)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gv)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // barManager1
+            // 
+            this.barManager1.Bars.AddRange(new DevExpress.XtraBars.Bar[] {
+            this.bar1,
+            this.bar3});
+            this.barManager1.DockControls.Add(this.barDockControlTop);
+            this.barManager1.DockControls.Add(this.barDockControlBottom);
+            this.barManager1.DockControls.Add(this.barDockControlLeft);
+            this.barManager1.DockControls.Add(this.barDockControlRight);
+            this.barManager1.Form = this;
+            this.barManager1.Items.AddRange(new DevExpress.XtraBars.BarItem[] {
+            this.addNewBtn,
+            this.editBtn,
+            this.trashBtn,
+            this.deleteBtn,
+            this.refreshBtn,
+            this.xlsBtn,
+            this.printBtn});
+            this.barManager1.MaxItemId = 7;
+            this.barManager1.RightToLeft = DevExpress.Utils.DefaultBoolean.True;
+            this.barManager1.StatusBar = this.bar3;
+            // 
+            // bar1
+            // 
+            this.bar1.BarName = "Tools";
+            this.bar1.DockCol = 0;
+            this.bar1.DockRow = 1;
+            this.bar1.DockStyle = DevExpress.XtraBars.BarDockStyle.Top;
+            this.bar1.LinksPersistInfo.AddRange(new DevExpress.XtraBars.LinkPersistInfo[] {
+            new DevExpress.XtraBars.LinkPersistInfo(DevExpress.XtraBars.BarLinkUserDefines.PaintStyle, this.addNewBtn, DevExpress.XtraBars.BarItemPaintStyle.CaptionGlyph),
+            new DevExpress.XtraBars.LinkPersistInfo(DevExpress.XtraBars.BarLinkUserDefines.PaintStyle, this.editBtn, DevExpress.XtraBars.BarItemPaintStyle.CaptionGlyph),
+            new DevExpress.XtraBars.LinkPersistInfo(DevExpress.XtraBars.BarLinkUserDefines.PaintStyle, this.trashBtn, "", true, true, true, 0, null, DevExpress.XtraBars.BarItemPaintStyle.CaptionGlyph),
+            new DevExpress.XtraBars.LinkPersistInfo(DevExpress.XtraBars.BarLinkUserDefines.PaintStyle, this.deleteBtn, DevExpress.XtraBars.BarItemPaintStyle.CaptionGlyph),
+            new DevExpress.XtraBars.LinkPersistInfo(DevExpress.XtraBars.BarLinkUserDefines.PaintStyle, this.refreshBtn, "", true, true, true, 0, null, DevExpress.XtraBars.BarItemPaintStyle.CaptionGlyph),
+            new DevExpress.XtraBars.LinkPersistInfo(this.xlsBtn, true),
+            new DevExpress.XtraBars.LinkPersistInfo(this.printBtn, true)});
+            this.bar1.OptionsBar.AllowQuickCustomization = false;
+            this.bar1.OptionsBar.DrawDragBorder = false;
+            this.bar1.OptionsBar.UseWholeRow = true;
+            this.bar1.Text = "Tools";
+            // 
+            // bar3
+            // 
+            this.bar3.BarName = "Status bar";
+            this.bar3.CanDockStyle = DevExpress.XtraBars.BarCanDockStyle.Bottom;
+            this.bar3.DockCol = 0;
+            this.bar3.DockRow = 0;
+            this.bar3.DockStyle = DevExpress.XtraBars.BarDockStyle.Bottom;
+            this.bar3.OptionsBar.AllowQuickCustomization = false;
+            this.bar3.OptionsBar.DrawDragBorder = false;
+            this.bar3.OptionsBar.UseWholeRow = true;
+            this.bar3.Text = "Status bar";
+            // 
+            // barDockControlTop
+            // 
+            this.barDockControlTop.CausesValidation = false;
+            this.barDockControlTop.Dock = System.Windows.Forms.DockStyle.Top;
+            this.barDockControlTop.Location = new System.Drawing.Point(0, 0);
+            this.barDockControlTop.Manager = this.barManager1;
+            this.barDockControlTop.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
+            this.barDockControlTop.Size = new System.Drawing.Size(737, 31);
+            // 
+            // barDockControlBottom
+            // 
+            this.barDockControlBottom.CausesValidation = false;
+            this.barDockControlBottom.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.barDockControlBottom.Location = new System.Drawing.Point(0, 432);
+            this.barDockControlBottom.Manager = this.barManager1;
+            this.barDockControlBottom.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
+            this.barDockControlBottom.Size = new System.Drawing.Size(737, 17);
+            // 
+            // barDockControlLeft
+            // 
+            this.barDockControlLeft.CausesValidation = false;
+            this.barDockControlLeft.Dock = System.Windows.Forms.DockStyle.Left;
+            this.barDockControlLeft.Location = new System.Drawing.Point(0, 31);
+            this.barDockControlLeft.Manager = this.barManager1;
+            this.barDockControlLeft.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
+            this.barDockControlLeft.Size = new System.Drawing.Size(0, 401);
+            // 
+            // barDockControlRight
+            // 
+            this.barDockControlRight.CausesValidation = false;
+            this.barDockControlRight.Dock = System.Windows.Forms.DockStyle.Right;
+            this.barDockControlRight.Location = new System.Drawing.Point(737, 31);
+            this.barDockControlRight.Manager = this.barManager1;
+            this.barDockControlRight.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
+            this.barDockControlRight.Size = new System.Drawing.Size(0, 401);
+            // 
+            // addNewBtn
+            // 
+            this.addNewBtn.Caption = "جدید";
+            this.addNewBtn.Id = 0;
+            this.addNewBtn.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("addNewBtn.ImageOptions.Image")));
+            this.addNewBtn.ImageOptions.LargeImage = ((System.Drawing.Image)(resources.GetObject("addNewBtn.ImageOptions.LargeImage")));
+            this.addNewBtn.Name = "addNewBtn";
+            // 
+            // editBtn
+            // 
+            this.editBtn.Caption = "ویرایش";
+            this.editBtn.Id = 1;
+            this.editBtn.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("editBtn.ImageOptions.Image")));
+            this.editBtn.ImageOptions.LargeImage = ((System.Drawing.Image)(resources.GetObject("editBtn.ImageOptions.LargeImage")));
+            this.editBtn.Name = "editBtn";
+            // 
+            // trashBtn
+            // 
+            this.trashBtn.Caption = "زباله دان";
+            this.trashBtn.Id = 2;
+            this.trashBtn.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("trashBtn.ImageOptions.Image")));
+            this.trashBtn.ImageOptions.LargeImage = ((System.Drawing.Image)(resources.GetObject("trashBtn.ImageOptions.LargeImage")));
+            this.trashBtn.Name = "trashBtn";
+            // 
+            // deleteBtn
+            // 
+            this.deleteBtn.Caption = "حذف";
+            this.deleteBtn.Id = 3;
+            this.deleteBtn.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("deleteBtn.ImageOptions.Image")));
+            this.deleteBtn.ImageOptions.LargeImage = ((System.Drawing.Image)(resources.GetObject("deleteBtn.ImageOptions.LargeImage")));
+            this.deleteBtn.Name = "deleteBtn";
+            // 
+            // refreshBtn
+            // 
+            this.refreshBtn.Caption = "نوسازی جدول";
+            this.refreshBtn.Id = 4;
+            this.refreshBtn.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("refreshBtn.ImageOptions.Image")));
+            this.refreshBtn.ImageOptions.LargeImage = ((System.Drawing.Image)(resources.GetObject("refreshBtn.ImageOptions.LargeImage")));
+            this.refreshBtn.Name = "refreshBtn";
+            // 
+            // xlsBtn
+            // 
+            this.xlsBtn.Alignment = DevExpress.XtraBars.BarItemLinkAlignment.Right;
+            this.xlsBtn.Caption = "اکسل";
+            this.xlsBtn.Id = 5;
+            this.xlsBtn.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("xlsBtn.ImageOptions.Image")));
+            this.xlsBtn.ImageOptions.LargeImage = ((System.Drawing.Image)(resources.GetObject("xlsBtn.ImageOptions.LargeImage")));
+            this.xlsBtn.Name = "xlsBtn";
+            // 
+            // printBtn
+            // 
+            this.printBtn.Alignment = DevExpress.XtraBars.BarItemLinkAlignment.Right;
+            this.printBtn.Caption = "چاپ";
+            this.printBtn.Id = 6;
+            this.printBtn.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("printBtn.ImageOptions.Image")));
+            this.printBtn.ImageOptions.LargeImage = ((System.Drawing.Image)(resources.GetObject("printBtn.ImageOptions.LargeImage")));
+            this.printBtn.Name = "printBtn";
+            // 
+            // gc
+            // 
+            this.gc.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gc.Location = new System.Drawing.Point(0, 31);
+            this.gc.MainView = this.gv;
+            this.gc.MenuManager = this.barManager1;
+            this.gc.Name = "gc";
+            this.gc.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.gc.Size = new System.Drawing.Size(737, 401);
+            this.gc.TabIndex = 4;
+            this.gc.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
+            this.gv});
+            // 
+            // gv
+            // 
+            this.gv.GridControl = this.gc;
+            this.gv.Name = "gv";
+            this.gv.OptionsView.ShowGroupPanel = false;
+            // 
+            // UnitList
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(737, 449);
+            this.Controls.Add(this.gc);
+            this.Controls.Add(this.barDockControlLeft);
+            this.Controls.Add(this.barDockControlRight);
+            this.Controls.Add(this.barDockControlBottom);
+            this.Controls.Add(this.barDockControlTop);
+            this.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
+            this.Name = "UnitList";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "لیست واحدهای کارگاه";
+            this.Load += new System.EventHandler(this.UnitList_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.barManager1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gc)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gv)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraBars.BarManager barManager1;
+        private DevExpress.XtraBars.Bar bar1;
+        private DevExpress.XtraBars.Bar bar3;
+        private DevExpress.XtraBars.BarDockControl barDockControlTop;
+        private DevExpress.XtraBars.BarDockControl barDockControlBottom;
+        private DevExpress.XtraBars.BarDockControl barDockControlLeft;
+        private DevExpress.XtraBars.BarDockControl barDockControlRight;
+        private DevExpress.XtraBars.BarButtonItem addNewBtn;
+        private DevExpress.XtraBars.BarButtonItem editBtn;
+        private DevExpress.XtraBars.BarButtonItem trashBtn;
+        private DevExpress.XtraBars.BarButtonItem deleteBtn;
+        private DevExpress.XtraBars.BarButtonItem refreshBtn;
+        private DevExpress.XtraBars.BarButtonItem xlsBtn;
+        private DevExpress.XtraBars.BarButtonItem printBtn;
+        private DevExpress.XtraGrid.GridControl gc;
+        private DevExpress.XtraGrid.Views.Grid.GridView gv;
+    }
+}
---
+++ ProduceRecovery/UnitList.cs
@@ -0,0 +1,26 @@
+﻿using DevExpress.XtraBars;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProduceRecovery
+{
+    public partial class UnitList : DevExpress.XtraEditors.XtraForm
+    {
+        public UnitList()
+        {
+            InitializeComponent();
+        }
+
+        private void UnitList_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
